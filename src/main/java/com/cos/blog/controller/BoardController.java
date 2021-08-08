0b40056b1122a3a2354c65a2893871faef5c116b
신승혁 <Cozak92@gmail.com>
2021-08-09 01:19:37 +0900

    메인 화면 글목록 구현 1

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,18 +1,24 @@
 package com.cos.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.blog.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 
 	
+	@Autowired
+	private BoardService boardService;
 //	@AuthenticationPrincipal PrincipalDetail principal <- 세션 찾는법
 		@GetMapping("/")
-		public String index() {
+		public String index(Model model) {
+			model.addAttribute("boards", boardService.글목록());
 			return "index";
 		}
 		
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,6 +1,8 @@
 package com.cos.blog.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,6 +26,10 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepositoy.save(board);
+	}
+	
+	public List<Board> 글목록(){
+		return boardRepositoy.findAll();
 	}
 }
 
