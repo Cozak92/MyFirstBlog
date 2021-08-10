86ece528c0a44456b2d21db9da832ba631c341e3
신승혁 <Cozak92@gmail.com>
2021-08-11 00:19:13 +0900

    메인 어플리케이션 수정
    EC2 테스트용

--- src/main/java/com/cos/blog/BlogApplication.java
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -1,10 +1,18 @@
 package com.cos.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer   {
+	
+	
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { 
+		return builder.sources(BlogApplication.class); 
+		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
